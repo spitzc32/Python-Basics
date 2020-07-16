b66cc21cdb7ba1c8036134cdd20a4e8627e132fd
spitzc32 <33305108+spitzc32@users.noreply.github.com>
2020-07-16 15:00:25 +0800

    Update exercise3.py

--- basic_python/exercises/exercises/exercise3.py
+++ basic_python/exercises/exercises/exercise3.py
@@ -4,3 +4,8 @@
 # and prints that string 3 times in a row.
 # So if I passed in the string "hello",
 # it would print "hellohellohello"
+
+def tripleprint(string):
+	print(string * 3)
+
+tripleprint("hello")
