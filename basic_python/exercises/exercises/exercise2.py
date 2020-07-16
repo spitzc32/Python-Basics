0b7c100f6c42c0a2becde523f6855ebecf9fa072
spitzc32 <33305108+spitzc32@users.noreply.github.com>
2020-07-16 14:59:52 +0800

    Update exercise2.py

--- basic_python/exercises/exercises/exercise2.py
+++ basic_python/exercises/exercises/exercise2.py
@@ -7,3 +7,8 @@
 # "You are before or after the 21st century"
 
 year = 1830
+
+if year >= 2000 and year <= 2100:
+	print("Welcome to the 21st century")
+else:
+	print("You are before or after the 21st century")
