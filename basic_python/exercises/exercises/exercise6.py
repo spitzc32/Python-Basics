557ed7d8b0fc75d52507924a21e67c1e16b7c8e1
spitzc32 <33305108+spitzc32@users.noreply.github.com>
2020-07-16 15:02:18 +0800

    Update exercise6.py

--- basic_python/exercises/exercises/exercise6.py
+++ basic_python/exercises/exercises/exercise6.py
@@ -7,3 +7,7 @@
 
 words = ["PoGo","Spange","Lie-Fi"]
 definitions = ["Slang for Pokemon Go","To collect spare change, either from couches, passerbys on the street or any numerous other ways and means","When your phone or tablet indicates that you are connected to a wireless network, however you are still unable to load webpages or use any internet services with your device"]
+
+cooldictionary = dict(zip(words,definitions))
+
+print(cooldictionary)
